fa02a402695379fa15bbe2be193a2031d44032f7
lgcarrilloc <mgoluiswar@hotmail.com>
2026-04-14 18:34:33 -0500

    Corrige inicialización del canvas y movimiento a la izquierda

--- cazando.js
+++ cazando.js
@@ -1,7 +1,7 @@
 // Canvas y contexto
-let canvas = document.getElementById("areaJuego");
-let ctx = canvas.getContext("2d");
-// Variables (inicializadas en 0)
+let canvas;
+let ctx;
+// Variables 
 let gatoX = 0;
 let gatoY = 0;
 let comidaX = 0;
@@ -12,34 +12,35 @@ const ALTO_GATO = 50;
 const ANCHO_COMIDA = 30;
 const ALTO_COMIDA = 30;
 const VELOCIDAD = 15;
-// Función genérica
+// Función rectangulos
 function graficarRectangulo(x, y, ancho, alto, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, ancho, alto);
 }
 // Dibujar gato
 function graficarGato() {
-  graficarRectangulo(
-    gatoX,
-    gatoY,
-    ANCHO_GATO,
-    ALTO_GATO,
-    "#000000"
-  );
+  graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "#000000");
 }
 // Dibujar comida
 function graficarComida() {
-  graficarRectangulo(
-    comidaX,
-    comidaY,
-    ANCHO_COMIDA,
-    ALTO_COMIDA,
-    "#00ff00"
-  );
+  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "#00ff00" );
+}
+// funcion limpiar canvas
+function limpiarCanva() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+// Movimiento del gato
+function moverIzquierda() {
+  gatoX = gatoX - 10;
+  limpiarCanva();
+  graficarGato();
+  graficarComida();
 }
 // Iniciar juego 
 function iniciarJuego() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas = document.getElementById("areaJuego");
+  ctx = canvas.getContext("2d");
+
   // Gato centrado
   gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
   gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
@@ -49,18 +50,5 @@ function iniciarJuego() {
   graficarGato();
   graficarComida();
 }
-// Movimiento del gato
-function mover(direccion) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if (direccion === "arriba") gatoY -= VELOCIDAD;
-  if (direccion === "abajo") gatoY += VELOCIDAD;
-  if (direccion === "izquierda") gatoX -= VELOCIDAD;
-  if (direccion === "derecha") gatoX += VELOCIDAD;
-  graficarGato();
-  graficarComida();
-}
 // Eventos de botones
-document.getElementById("btnArriba").onclick = () => mover("arriba");
-document.getElementById("btnAbajo").onclick = () => mover("abajo");
-document.getElementById("btnIzquierda").onclick = () => mover("izquierda");
-document.getElementById("btnDerecha").onclick = () => mover("derecha");
+document.getElementById("btnIzquierda").onclick = moverIzquierda;
