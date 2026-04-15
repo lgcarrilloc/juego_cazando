cffdd6f6559cbf1daf6e1d9a7f87199f628edb70
lgcarrilloc <mgoluiswar@hotmail.com>
2026-04-14 21:33:10 -0500

    Agregar  victoria, game over y reinicio del juego

--- cazando.js
+++ cazando.js
@@ -3,6 +3,7 @@ let canvas;
 let ctx;
 let puntaje= 0;
 let tiempo=10;
+let intervaloTiempo;
 // Variables 
 let gatoX = 0;
 let gatoY = 0;
@@ -74,14 +75,16 @@ function iniciarJuego() {
   comidaY = canvas.height - ALTO_COMIDA;
   graficarGato();
   graficarComida();
+  moverComidaAleatoria();
+  // Eventos de botones
+    document.getElementById("btnIzquierda").onclick = moverIzquierda;
+    document.getElementById("btnDerecha").onclick = moverDerecha;
+    document.getElementById("btnArriba").onclick = moverArriba;
+    document.getElementById("btnAbajo").onclick = moverAbajo;
+    document.getElementById("btnReiniciar").onclick = reiniciarJuego;
   //conteo regresivo cada segundo 
-setInterval(restarTiempo, 1000);
+intervaloTiempo=setInterval(restarTiempo, 1000);
 }
-// Eventos de botones
-document.getElementById("btnIzquierda").onclick = moverIzquierda;
-document.getElementById("btnDerecha").onclick = moverDerecha;
-document.getElementById("btnArriba").onclick = moverArriba;
-document.getElementById("btnAbajo").onclick = moverAbajo;
 //funcion detectar colisión
 function detectarColision(){
     if(gatoX < comidaX + ANCHO_COMIDA && gatoX + ANCHO_GATO > comidaX &&
@@ -89,7 +92,6 @@ function detectarColision(){
     ) {
         moverComidaAleatoria();
         aumentarPuntaje();
-
         limpiarCanva();
         graficarGato();
         graficarComida();
@@ -102,8 +104,30 @@ function moverComidaAleatoria(){
 function aumentarPuntaje(){
     puntaje++;
     mostrarEnSpan("puntos", puntaje);
+    if(puntaje >= 6){
+        clearInterval(intervaloTiempo);
+        alert("¡Ganaste!");
+    }
 }
 function restarTiempo(){
     tiempo--;
     mostrarEnSpan("tiempo", tiempo);
+    if(tiempo <= 0){
+        clearInterval(intervaloTiempo);
+        alert("GAme Over");
+    }
+}
+function reiniciarJuego(){
+    clearInterval(intervaloTiempo);
+    //reiniciar variables
+    tiempo = 10;
+    puntaje = 0;
+    mostrarEnSpan("tiempo", tiempo);
+    mostrarEnSpan("puntos", puntaje);
+    gatoX = (canvas.width - ANCHO_GATO / 2);
+    gatoY = (canvas.height - ALTO_GATO / 2);
+    moverComidaAleatoria();
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
 }
